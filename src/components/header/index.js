e43dad0f5c041212d9ba9f3838934ea1952817b9
Nick Kasten <kastencode@gmail.com>
2017-08-07 21:29:27 -0500

    removing boilerplate code. adding test component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,10 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router/match';
+import { h } from 'preact';
 import style from './style';
 
-export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
+export default function Header(props) {
+	return (
+		<header class={style.header}>
+			<h1>Shopping List</h1>
+		</header>
+	);
 }
---
+++ src/components/test/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import style from './style';
+
+export default function Test(props) {
+  return (
+    <div class={style.testDiv}>
+      TEST
+    </div>
+  );
+}
