8abde5bc4e9e64ebc98798d231ab90d2de9233ef
Nick Kasten <kastentx@gmail.com>
2017-08-08 16:47:42 -0500

    refactored Item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -4,7 +4,10 @@ import style from './style';
 export default function Item(props) {
   return (
     <div class={style.test}>
-      <li>{props.text}<span class={style.delBtn} onClick={props.delDoc} data-item={props.text} data-id={props.id}>X</span></li>
+      <li>
+        {props.text}
+        <span class={style.delBtn} onClick={props.delDoc} data-item={props.text} data-id={props.id}>X</span>
+      </li>
     </div>
   );
 }
