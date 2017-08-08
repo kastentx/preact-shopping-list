63fb8aebbdefb5d4bc2731c929ac1419ff753d4e
Nick Kasten <kastentx@gmail.com>
2017-08-08 16:44:45 -0500

    using standard <li> element

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,12 +1,10 @@
 import preact from 'preact';
-import List from 'preact-material-components/List';
-import 'preact-material-components/List/style.css';
 import style from './style';
 
 export default function Item(props) {
   return (
-    <div>
-      <List.item>{props.text} <span class={style.delBtn} onClick={props.delDoc} data-item={props.text} data-id={props.id}>X</span></List.item>
+    <div class={style.test}>
+      <li>{props.text}<span class={style.delBtn} onClick={props.delDoc} data-item={props.text} data-id={props.id}>X</span></li>
     </div>
   );
 }
